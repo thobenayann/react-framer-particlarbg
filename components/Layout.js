5180eb7adc5243fb900f538484a41e3fd6a8379e
thobenayann <thobena.yann@orange.fr>
2023-09-02 10:43:57 +0200

    layout and navigation

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,25 @@
-const Layout = () => {
-  return <div>Layout</div>;
+// fonts
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({
+    weight: ['100', '300', '400', '500', '700', '900'],
+    subsets: ['latin'],
+});
+
+// Components
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+
+const Layout = ({ children }) => {
+    return (
+        <div
+            className={`page bg-site text-white bg-cover bg-no-repeat ${roboto.className} font-roboto relative`}
+        >
+            <Nav />
+            <Header />
+            {children}
+        </div>
+    );
 };
 
 export default Layout;
