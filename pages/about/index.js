82be2604df95cb6d0105c872db65ad07f3f68ad9
thobenayann <thobena.yann@orange.fr>
2023-09-04 19:49:57 +0200

    add id in aboutData variable

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,95 +1,112 @@
 // icons
 import {
-  FaHtml5,
-  FaCss3,
-  FaJs,
-  FaReact,
-  FaWordpress,
-  FaFigma,
-} from "react-icons/fa";
+    FaHtml5,
+    FaCss3,
+    FaJs,
+    FaReact,
+    FaWordpress,
+    FaFigma,
+} from 'react-icons/fa';
 
 import {
-  SiNextdotjs,
-  SiFramer,
-  SiAdobexd,
-  SiAdobephotoshop,
-} from "react-icons/si";
-
+    SiNextdotjs,
+    SiFramer,
+    SiAdobexd,
+    SiAdobephotoshop,
+} from 'react-icons/si';
 
 //  data
 const aboutData = [
-  {
-    title: 'skills',
-    info: [
-      {
-        title: 'Web Development',
-        icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
+    {
+        id: 1,
+        title: 'skills',
+        info: [
+            {
+                id: 1,
+                title: 'Web Development',
+                icons: [
+                    { id: 1, component: <FaHtml5 /> },
+                    { id: 2, component: <FaCss3 /> },
+                    { id: 3, component: <FaJs /> },
+                    { id: 4, component: <FaReact /> },
+                    { id: 5, component: <SiNextdotjs /> },
+                    { id: 6, component: <SiFramer /> },
+                    { id: 7, component: <FaWordpress /> },
+                ],
+            },
+            {
+                id: 2,
+                title: 'UI/UX Design',
+                icons: [
+                    { id: 1, component: <FaFigma /> },
+                    { id: 2, component: <SiAdobexd /> },
+                    { id: 3, component: <SiAdobephotoshop /> },
+                ],
+            },
         ],
-      },
-      {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
-    ],
-  },
-  {
-    title: 'awards',
-    info: [
-      {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
-      },
-      {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'experience',
-    info: [
-      {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
-      },
-      {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
-      },
-      {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'credentials',
-    info: [
-      {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
-      },
-      {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
-      },
-      {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
-      },
-    ],
-  },
+    },
+    {
+        id: 2,
+        title: 'awards',
+        info: [
+            {
+                id: 1,
+                title: 'Webby Awards - Honoree',
+                stage: '2011 - 2012',
+            },
+            {
+                id: 2,
+                title: 'Adobe Design Achievement Awards - Finalist',
+                stage: '2009 - 2010',
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: 'experience',
+        info: [
+            {
+                id: 1,
+                title: 'UX/UI Designer - XYZ Company',
+                stage: '2012 - 2023',
+            },
+            {
+                id: 2,
+                title: 'Web Developer - ABC Agency',
+                stage: '2010 - 2012',
+            },
+            {
+                id: 3,
+                title: 'Intern - DEF Corporation',
+                stage: '2008 - 2010',
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: 'credentials',
+        info: [
+            {
+                id: 1,
+                title: 'Web Development - ABC University, LA, CA',
+                stage: '2011',
+            },
+            {
+                id: 2,
+                title: 'Computer Science Diploma - AV Technical Institute',
+                stage: '2009',
+            },
+            {
+                id: 3,
+                title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
+                stage: '2006',
+            },
+        ],
+    },
 ];
 
 const About = () => {
-  return <div>About</div>;
+    return <div>About</div>;
 };
 
 export default About;
