54a144fed02c126f6d2e2d3ec092803deecc408a
thobenayann <thobena.yann@orange.fr>
2023-09-04 19:37:04 +0200

    fix index issue

--- components/Nav.js
+++ components/Nav.js
@@ -10,20 +10,17 @@ import {
 
 // nav data
 export const navData = [
-    { name: 'Accueil', path: '/', icon: <HiHome /> },
+    { id: 11, name: 'Accueil', path: '/', icon: <HiHome /> },
     {
+        id: 12,
         name: 'Témoignages',
         path: '/testimonials',
         icon: <HiChatBubbleBottomCenterText />,
     },
-    { name: 'Services', path: '/services', icon: <HiRectangleGroup /> },
-    { name: 'Réalisations', path: '/work', icon: <HiViewColumns /> },
-    { name: 'Mon histoire', path: '/about', icon: <HiUser /> },
-    {
-        name: 'Contact',
-        path: '/contact',
-        icon: <HiEnvelope />,
-    },
+    { id: 13, name: 'Services', path: '/services', icon: <HiRectangleGroup /> },
+    { id: 14, name: 'Réalisations', path: '/work', icon: <HiViewColumns /> },
+    { id: 15, name: 'Mon histoire', path: '/about', icon: <HiUser /> },
+    { id: 16, name: 'Contact', path: '/contact', icon: <HiEnvelope /> },
 ];
 
 // Next link
@@ -39,7 +36,7 @@ const Nav = () => {
             {/* Desktop NAV */}
             <nav className='fixed z-50 right-0 bg-white/10 w-full'>
                 <div className='hidden xl:flex w-full items-center justify-end gap-x-10 px-4 h-max py-4 bg-transparent backdrop-blur-sm text-xl'>
-                    {navData.map((link, index) => {
+                    {navData.map((link) => {
                         return (
                             <Link
                                 className={`relative items-center group hover:text-blue-500 transition-all duration-300 ${
@@ -50,7 +47,7 @@ const Nav = () => {
                                         : ''
                                 }`}
                                 href={link.path}
-                                key={index}
+                                key={link.id}
                             >
                                 {/* Links */}
                                 {link.name === 'Accueil' ? (
@@ -68,14 +65,14 @@ const Nav = () => {
             <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:hidden'>
                 {/* inner */}
                 <div className='flex w-full xl:flex-col items-center justify-between gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
-                    {navData.map((link, index) => {
+                    {navData.map((link) => {
                         return (
                             <Link
                                 className={`${
                                     link.path === pathname && 'text-blue-500'
                                 } relative items-center group hover:text-blue-500 transition-all duration-300`}
                                 href={link.path}
-                                key={index}
+                                key={link.id}
                             >
                                 {/* tooltip */}
                                 <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
